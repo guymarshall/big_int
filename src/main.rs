b914e77d6f7bcb9f85269d10d6f45e9eded66418
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-11-21 12:31:56 +0000

    Add BigInt struct and impl get_digits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug)]
+struct BigInt {
+    value: String
+}
+
+impl BigInt {
+    fn get_digits(&self) -> Vec<u32> {
+        self.value.chars().flat_map(|c| c.to_digit(10)).collect()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let huge_number: BigInt = BigInt {value: "725935728739872".to_string()};
+    let huge_number_digits: Vec<u32> = huge_number.get_digits();
+
+    println!("Huge number: {:?}, digits: {:?}", huge_number, huge_number_digits);
 }
